8b7d454ced4d521541a9a677703c981b099059ee
noumanahmadatempg <98813753+noumanahmadatempg@users.noreply.github.com>
2022-10-21 04:01:25 +0500

    send email command

---
+++ resources/views/emails/testing_email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# hi nomi
+
+This is just a testing email. Please ignore
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+@endcomponent
