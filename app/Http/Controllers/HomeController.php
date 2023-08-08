84bd97caa2b577304c055faafb45e94360bb1c89
noumanahmad448 <nouman.ahmad448@gmail.com>
2023-08-08 21:47:15 +0800

    local changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,6 @@ class HomeController extends Controller
             $post = Post::where('status', 'published')->select('id', 'title', 'message', 'upload_img', 'f_name', 'slug')->orderByDesc('created_at')->first();
             $faq = Faq::where('status', 'published')->select('id', 'title', 'message', 'upload_img', 'f_name', 'slug')->orderByDesc('created_at')->first();
             $courses = Course::where('status', 'published')->whereNull('is_deleted')->with(['price:id,course_id,pricing,is_free', 'user:id,name', 'course_image'])->select('id', 'user_id', 'course_title', 'categories_selection', 'slug')->orderByDesc('created_at')->paginate(20);
-
             return view(config("setting.welcome_blade"), compact('title', 'desc', 'cs', 'post', 'faq', 'courses'));
         } catch (Exception $e) {
             return back()->with('error', 'action cannot be performed now');
