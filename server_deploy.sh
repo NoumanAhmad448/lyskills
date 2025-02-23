4f7e8bb26fae03e80b80e60e33b9ae6f50b85dd4
Eren <nouman.laravel@outlook.com>
2025-02-23 14:41:15 +0800

    test

--- server_deploy.sh
+++ server_deploy.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 # Enable maintenance mode
@@ -59,59 +58,5 @@ chown root:root /home/nomilyskills/public_html/composer.json
 chown root:root /home/nomilyskills/public_html/composer.lock
 
 
-
 # Disable maintenance mode
 php artisan up
-=======
-# Turn on maintenance mode
-php artisan down || true
-
-# Install/update composer dependecies
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --no-cache
-
-# Clear caches
-php artisan cache:clear
-
-# Run database migrations
-php artisan migrate --force
-
-#generate artisan key
-php artisan key:generate
-
-# change folder permission
-chmod -R 777 storage bootstrap/cache
-
-# change folder permission
-sudo chown -R root:root /home/nomilyskills/public_html/
-
-# change node permission
-sudo ln -sf $(which node) /usr/bin/node
-sudo chmod +x $(which node)
-
-# Redisplay the maintenance mode message
-ls -lah $(which node)
-
-# Clear and cache routes
-php artisan route:cache
-
-# Clear and cache config
-php artisan config:cache
-
-# Clear and cache views
-php artisan view:cache
-
-# set the right version of node
-nvm use 16
-
-# Install node modules
-npm install
-
-# Build assets using Laravel Mix
-npm run production
-
-# clear permission
-sudo chown -R nomilyskills:nomilyskills /home/nomilyskills/public_html/
-
-# Turn off maintenance mode
-php artisan up
->>>>>>> e44f754a4554a0247a14670e5f5086e3e9cc916a
