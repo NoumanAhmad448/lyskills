57d5ba56c9d5fc635de6f87bbc50b29c081538aa
eren <nouman.laravel@outlook.com>
2025-03-08 16:16:32 +0800

    new test case added

--- tests/Feature/EnvFilesConsistencyTest.php
+++ tests/Feature/EnvFilesConsistencyTest.php
@@ -69,4 +69,45 @@ class EnvFilesConsistencyTest extends TestCase
 
         return $keys;
     }
+
+    private function checkDuplicateKeysInEnvFile($filePath)
+    {
+        if (!file_exists($filePath)) {
+            return true;
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $keys = [];
+        $duplicates = [];
+
+        foreach ($lines as $line) {
+            if (strpos($line, '=') !== false) {
+                list($key, ) = explode('=', $line, 2);
+                $key = trim($key);
+
+                if (isset($keys[$key])) {
+                    $duplicates[] = $key;
+                } else {
+                    $keys[$key] = true;
+                }
+            }
+        }
+        debug_logs(!empty($duplicates) ? $duplicates : "No duplicate keys found in $filePath.");
+        return empty($duplicates) ? true : $duplicates;
+    }
+    public function testCheckDuplicateKeysInEnvFile()
+    {
+        $envFiles = config("setting.env_files");
+
+        foreach ($envFiles as $envFile) {
+            $testFilePath = base_path($envFile);
+            $duplicates = $this->checkDuplicateKeysInEnvFile($testFilePath);
+            if (is_array($duplicates)) {
+                $this->fail("Test failed: Duplicate keys found in file $envFile - " . implode(', ', $duplicates));
+            }
+            $this->assertTrue($duplicates);
+        }
+    }
+
+
 }
