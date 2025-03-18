5e65b531fcc909c77678540e75812a747b042bbc
Eren <nouman.laravel@outlook.com>
2025-03-19 04:21:27 +0800

    blade formating

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,11 +38,8 @@ class HomeController extends Controller
             $courses = Course::where('status', 'published')->whereNull('is_deleted')->with(['price:id,course_id,pricing,is_free', 'user:id,name', 'course_image'])->select('id', 'user_id', 'course_title', 'categories_selection', 'slug')->orderByDesc('created_at')->paginate(20);
             return view(config("setting.welcome_blade"), compact('title', 'desc', 'cs', 'post', 'faq', 'courses',"settings","RatingModal"));
         } catch (Exception $e) {
-            if(config("app.debug")){
-                dd($e->getMessage());
-            }else{
-                return back()->with('error', __("messages.universal_err_msg"));
-            }
+            debug_logs($e->getMessage());
+            return back()->with('error', __("messages.universal_err_msg"));
         }
     }
 
